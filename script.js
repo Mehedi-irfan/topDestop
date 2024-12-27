8b3905c0261285fc01d601aa5941bef3572c1bcd
Mehedi-irfan <mehediirfan99@gmail.com>
2024-12-27 11:18:40 +0600

    navbar section added

---
+++ script.js
@@ -0,0 +1,4 @@
+const MobileNav = document.getElementById("mobile-nav");
+function handleClick() {
+  MobileNav.classList.toggle("hidden");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
+      screens: {
+        md: "990px",
+      },
+      colors: {
+        primary: "#3238f2",
+      },
       fontFamily: {
         popins: ["Poppins", "serif"],
         lobster: ["Lobster", "serif"],
